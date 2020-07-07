5c3d7ba4dc3469652637189ecefeafd1c67c14b8
chy <>
2020-07-07 18:41:19 +0800

    1

--- ServerLogic/ServerLogic.js
+++ ServerLogic/ServerLogic.js
@@ -2,7 +2,7 @@ var ws = require("nodejs-websocket");
 ws.createServer((socket)=>{
     console.log("a user connect");
     socket.on("text", (str)=>{
-        console.log("收到消息", JSON.stringify(data));
+        console.log("收到客户端发来的消息", str);
         let data = JSON.parse(str);
         let type = data.type;
         if (type === 'login'){
