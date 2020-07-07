d51e79f569d61cbf381afeda49468d1fb7925b48
chy <>
2020-07-07 18:55:35 +0800

    1

--- ServerLogic/ServerLogic.js
+++ ServerLogic/ServerLogic.js
@@ -1,12 +1,45 @@
 var ws = require("nodejs-websocket");
-ws.createServer((socket)=>{
+let DB = require("./db");
+let db = new DB();
+ws.createServer((socket) => {
     console.log("a user connect");
-    socket.on("text", (str)=>{
+    socket.on("text", (str) => {
         console.log("收到客户端发来的消息", str);
         let data = JSON.parse(str);
         let type = data.type;
-        if (type === 'login'){
+        let callBackIndex = data.callBackIndex;
+        if (type === 'login') {
             console.log('玩家登录');
+            db.login(data.id).then((err, result) => {
+                if (!err) {
+                    if (result.length === 0) {
+                        db.createUserInfo({
+                            id: id,
+                            name: data.name,
+                            money: 500,
+                            score: 0
+                        }).then(() => {
+                            socket.send(JSON.stringify({
+                                status: 'ok',
+                                result: {
+                                    id: data.id,
+                                    name: data.name,
+                                    money: 500,
+                                    score: 0
+                                },
+                                callBackIndex: callBackIndex
+                            }))
+                        })
+                    } else {
+                        socket.send(JSON.stringify({
+                            status: 'ok',
+                            result: result[0],
+                            callBackIndex: callBackIndex
+                        }))
+                    }
+                }
+            })
+
         }
 
 
---
+++ ServerLogic/db.js
@@ -0,0 +1,65 @@
+let mysql = require("mysql");
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "chu7758521",
+    database: "happy_life"
+});
+connection.connect();
+
+class DB {
+    constructor() {
+
+    }
+    login(id) {
+        let sql = "select * from playerinfo where id = " + id + ";";
+        console.log("seq", sql);
+        return new Promise((resolve, reject) => {
+            connection.query(sql, function (error, results, fields) {
+                // if (error) throw error;
+                // console.log('The solution is: ', results[0]);
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(null, results);
+                }
+            });
+        });
+
+    }
+    createUserInfo(data) {
+        let sql = "insert into playerinfo ("
+        let str = ''
+        let str2 = '';
+        for (let i in data) {
+            str += i + ' ,';
+            str2 += data[i] + ','
+        }
+        str = str.substring(0, str.length - 1);
+        str2 = str2.substring(0, str2.length - 1);
+        console.log("str", str);
+        console.log("str2", str2);
+        sql = sql + str + ") values (" + str2 + ");";
+        console.log("sql", sql);
+        return new Promise((resolve, reject) => {
+            connection.query(sql, (err, results, fields) => {
+                console.log("result", results);
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        })
+    }
+    getBuildInfo(){
+
+    }
+}
+module.exports = DB;
+//create table playerinfo(id int, money int, score int);
+//select from * playerinfo where id = 
+//ALTER TABLE menu_function ADD COLUMN sort VARCHAR(20) DEFAULT NULL COMMENT '排序字段'
+// alter table playerinfo add column name varchar(20) DEFAULT NULL COMMENT '无名氏'
+// create table houseinfo(ownerid varchar(20),ownername varchar(20),id int, state int,build_cost int);
+// 状态 food count 
