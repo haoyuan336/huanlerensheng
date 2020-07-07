9ca709b2612a339419cf0a7e5a5ec2da83465abd
chy <>
2020-07-07 18:40:22 +0800

    1

--- HappyLifeClient/assets/Script/Data/NetworkController.js
+++ HappyLifeClient/assets/Script/Data/NetworkController.js
@@ -3,50 +3,49 @@ let longSerUrl = "ws://120.53.243.146:3001";
 class NetworkController {
     constructor() {
         this._ws = undefined;
+        this._callBackIndex = 1;
+        this._callBackMap = {};
     }
-    connectLongServer(){
-        return new Promise((resolve)=>{
+    connectLongServer() {
+        return new Promise((resolve) => {
             this._ws = new WebSocket(longSerUrl);
-            this._ws.onopen = ()=>{
+            this._ws.onopen = () => {
                 console.log("链接成功");
                 resolve();
             };
-            this._ws.onmessage = (text)=>{
+            this._ws.onmessage = (text) => {
                 console.log("收到消息", text);
             };
-            this._ws.onerror = (err)=>{
+            this._ws.onerror = (err) => {
                 console.error('connect server ', err);
             };
-            this._ws.onclose = ()=>{
+            this._ws.onclose = () => {
                 console.log("on close");
             }
         });
     }
     loginServer(data) {
         console.log("登录服务器", data);
-        return new Promise((resolve)=>{
-            this.sendMessage('login', data);
+        return new Promise((resolve) => {
+            this.sendMessage('login', data).then((result)=>{
+                console.log("登录结果", result);
+            });
         });
-        // return fetch(serUrl + '/login',
-        //     {
-        //         method: "post",
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         },
-        //         body: JSON.stringify(data)
-        //     }).then((response) => {
-        //         return response.json();
-        //     }).then(() => {
-        //         return this.connectLongServer();
-        //     })
+
     }
-    sendMessage(type, data){
-        this._ws.send({
-            type: type,
-            data: data
+    sendMessage(type, data) {
+        return new Promise((resolve) => {
+            this._ws.send(JSON.stringify({
+                type: type,
+                data: data,
+                callBackIndex: this._callBackIndex
+            }));
+            this._callBackMap[this._callBackIndex] = resolve;
+            this._callBackIndex ++;
         })
+
     }
-   
+
     getGameConfig() {
         return fetch(serUrl + '/getGameConfig', {
             method: "get"
--- ServerLogic/ServerLogic.js
+++ ServerLogic/ServerLogic.js
@@ -1,7 +1,14 @@
 var ws = require("nodejs-websocket");
 ws.createServer((socket)=>{
     console.log("a user connect");
-    socket.on("text", (data)=>{
+    socket.on("text", (str)=>{
         console.log("收到消息", JSON.stringify(data));
+        let data = JSON.parse(str);
+        let type = data.type;
+        if (type === 'login'){
+            console.log('玩家登录');
+        }
+
+
     });
 }).listen(3001); 
